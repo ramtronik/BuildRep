8e2c4268e2850f41e92ccb39b71424a2d0deffe8
ramtronik <46787283+ramtronik@users.noreply.github.com>
2022-05-08 20:04:01 +0100

    Update jenkins.1.sh

--- jenkins.1.sh
+++ jenkins.1.sh
@@ -3,7 +3,10 @@ sudo apt update -y
 sudo apt 
 sudo apt install openjdk-11-jre -y
 sudo apt install curl -y
+sudo apt install vim -y
 
+#recent versions of Jenkins already add themselves to systemdto start up
+# oh, and set the jenkins home (/lib/systemd/system/jenkins.service)
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
